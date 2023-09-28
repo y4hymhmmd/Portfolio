a95dfa8a84e480b1f29e4bc170f8f6d1c1877bd9
Yahya Muhammad <firzasaputra08@gmail.com>
2023-09-28 11:41:40 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+/*==================== LOADING ====================*/
+
 /*==================== MENU SHOW Y HIDDEN ====================*/
 const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
@@ -94,6 +96,10 @@ modalCloses.forEach((modalCloses) => {
 let swiperPortfolio = new Swiper(".portfolio__container", {
   cssMode: true,
   loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -109,7 +115,10 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
   loop: true,
   grabCursor: true,
   spaceBetween: 48,
-
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -123,71 +132,117 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
 });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll("section[id]");
 
 function scrollActive() {
-  const scrollY = window.pageYOffset
+  const scrollY = window.pageYOffset;
 
-  sections.forEach(current => {
-    const sectionHeight = current.offsetHeight
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
     const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute('id')
+    sectionId = current.getAttribute("id");
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
     } else {
-      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
     }
-  })
+  });
 }
-window.addEventListener('scroll', scrollActive)
-
+window.addEventListener("scroll", scrollActive);
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 function scrollHeader() {
-  const nav = document.getElementById('header')
+  const nav = document.getElementById("header");
   // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-  if (this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
 }
-window.addEventListener('scroll', scrollHeader)
-
+window.addEventListener("scroll", scrollHeader);
 
 /*==================== SHOW SCROLL UP ====================*/
 function scrollUp() {
-  const scrollUp = document.getElementById('scroll-up');
+  const scrollUp = document.getElementById("scroll-up");
   // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-  if (this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
 }
-window.addEventListener('scroll', scrollUp)
-
+window.addEventListener("scroll", scrollUp);
 
 /*==================== DARK LIGHT THEME ====================*/
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'uil-sun'
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "uil-sun";
 
 // Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
 // We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme,
+  );
+  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
+    iconTheme,
+  );
 }
 
 // Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
+themeButton.addEventListener("click", () => {
   // Add or remove the dark / icon theme
-  document.body.classList.toggle(darkTheme)
-  themeButton.classList.toggle(iconTheme)
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
   // We save the theme and the current icon that the user chose
-  localStorage.setItem('selected-theme', getCurrentTheme())
-  localStorage.setItem('selected-icon', getCurrentIcon())
-})
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
 
+/*==================== TYPED ====================*/
+const textList = ["Yahya", "Student", "Dreamer", "Programmer"]; // Daftar teks yang akan diubah-ubah
+let currentTextIndex = 0;
+let currentCharacterIndex = 0;
+const typingSpeed = 100; // Kecepatan pengetikan dalam milidetik
+
+function typeNextCharacter() {
+  const typingText = document.getElementById("typing-text");
+  const currentText = textList[currentTextIndex];
+  typingText.textContent = currentText.substring(0, currentCharacterIndex);
+  currentCharacterIndex++;
+
+  if (currentCharacterIndex <= currentText.length) {
+    setTimeout(typeNextCharacter, typingSpeed);
+  } else {
+    // Teks saat ini telah selesai diketikkan, pindah ke teks berikutnya
+    setTimeout(eraseText, 2000); // Tunggu 2 detik sebelum menghapus teks
+  }
+}
+
+function eraseText() {
+  const typingText = document.getElementById("typing-text");
+  const currentText = textList[currentTextIndex];
+  typingText.textContent = currentText.substring(0, currentCharacterIndex);
+  currentCharacterIndex--;
+
+  if (currentCharacterIndex >= 0) {
+    setTimeout(eraseText, typingSpeed);
+  } else {
+    // Penghapusan selesai, lanjut ke teks berikutnya
+    currentTextIndex = (currentTextIndex + 1) % textList.length;
+    setTimeout(typeNextCharacter, 500); // Tunggu 0.5 detik sebelum mengetikkan teks baru
+  }
+}
+
+// Mulai dengan mengetikkan teks pertama
+setTimeout(typeNextCharacter, 1000); // Tunggu 1 detik sebelum memulai pengetikan
